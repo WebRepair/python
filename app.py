1a50948366097d09710af02b15126d9ebccad287
WebRepair <108232179+WebRepair@users.noreply.github.com>
2023-07-09 17:23:08 +0200

    netlify fix1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, send_from_directory
 import subprocess
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
